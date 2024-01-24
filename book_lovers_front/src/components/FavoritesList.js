bd18a30f0ff2ef147b8d670b012d4d0e133b326c
neamahmami <79372921+neamahmami@users.noreply.github.com>
2024-01-24 16:28:18 +0100

    Showfavoritesfront

--- book_lovers_front/src/components/FavoritesList.js
+++ book_lovers_front/src/components/FavoritesList.js
@@ -1,40 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import { Table } from 'react-bootstrap';
 import axios from 'axios';
+import BookDetails from './BookDetails'; 
 
 const BookList = () => {
     const [books, setBooks] = useState([]);
 
     useEffect(() => {
         // Effectuez une requête HTTP pour récupérer la liste des livres depuis votre backend
-        axios.get('http://localhost:8080/api/favorites')
-            .then(response => setBooks(response.data))
-            .catch(error => console.error('Erreur lors de la récupération des livres:', error));
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('http://localhost:8080/api/favorites');
+                setBooks(response.data);
+            } catch (error) {
+                console.error('Erreur lors de la récupération des livres:', error);
+            }
+        };
+        fetchData(); // Appelez la fonction asynchrone ici
     }, []);
 
     return (
         <div>
-            <h2>Liste des Livres favoris</h2>
-            <Table striped bordered hover>
-                <thead>
-                <tr>
-                    <th>Titre</th>
-                    <th>Auteur</th>
-                    <th>Description</th>
-                    {/* Ajoutez d'autres colonnes en fonction de vos données */}
-                </tr>
-                </thead>
-                <tbody>
-                {books.map(book => (
-                    <tr key={book.id}>
-                        <td>{book.title}</td>
-                        <td>{book.author}</td>
-                        <td>{book.description}</td>
-                        {/* Ajoutez d'autres colonnes en fonction de vos données */}
-                    </tr>
-                ))}
-                </tbody>
-            </Table>
+            <h2>Liste des Livres</h2>
+            {books.map(book => (
+                <BookDetails key={book.id} bookInfo={book} />
+            ))}
         </div>
     );
 };
+
+export default BookList;
